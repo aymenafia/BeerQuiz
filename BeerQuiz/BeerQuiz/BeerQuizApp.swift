defdc2579a0629c936eb7c3615cd93d04103dea0
aymenafia <mohamedaymen.afia@esprit.tn>
2022-05-25 12:50:04 +0200

    create xCode project

---
+++ BeerQuiz/BeerQuiz/BeerQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerQuizApp.swift
+//  BeerQuiz
+//
+//  Created by Aymen on 25.05.22.
+//
+
+import SwiftUI
+
+@main
+struct BeerQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
