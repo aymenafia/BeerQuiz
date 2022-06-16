27b5dadadada5707368564414bfc8b1628279931
Aymen Aifa <aymen.aifa@thinxnet.com>
2022-06-16 16:43:08 +0200

    questionView and answers view implemented with Model and manager

---
+++ BeerQuiz/BeerQuiz/Components/PrimaryButton.swift
@@ -0,0 +1,30 @@
+//
+//  PrimaryButton.swift
+//  BeerQuiz
+//
+//  Created by Aymen Aifa on 16.06.22.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var text: String
+    var background: Color = Color("AccentColor")
+    
+    
+    var body: some View {
+        Text(text)
+            .foregroundColor(.white)
+            .padding()
+            .padding(.horizontal)
+            .background(background)
+            .cornerRadius(30)
+            .shadow(radius: 10)
+    }
+}
+
+struct PrimaryButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryButton(text: "Next")
+    }
+}
---
+++ BeerQuiz/BeerQuiz/Models/Answer.swift
@@ -0,0 +1,14 @@
+//
+//  Answer.swift
+//  BeerQuiz
+//
+//  Created by Aymen Aifa on 16.06.22.
+//
+
+import Foundation
+
+struct Answer: Identifiable {
+    var id = UUID() // Setting the UUID ourselves inside of the model, because API doesn't return a unique ID for each answer
+    var text: AttributedString
+    var isCorrect: Bool
+}
